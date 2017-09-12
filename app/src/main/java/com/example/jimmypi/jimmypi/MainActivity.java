201d3bde33cff4d1bf6ecaf5ab8f1c9964749110
Unknown <chivo.sergio.jesus@gmail.com>
2017-09-12 13:54:25 +0200

    Added reconocimiento de voz

---
+++ app/src/main/java/com/example/jimmypi/jimmypi/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.jimmypi.jimmypi;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    // constante NPI
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button BtnGps = (Button) findViewById(R.id.BtnGps);
+        Button BtnLlamar = (Button) findViewById(R.id.BtnLlamar);
+        Button BtnMusica = (Button) findViewById(R.id.BtnMusica);
+        Button BtnVoz = (Button) findViewById(R.id.BtnVoz);
+
+
+        // Escuchador evento boton central para recoger voz
+        BtnVoz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EscucharVoz();
+            }
+        });
+
+    }
+
+    /**
+     * Muestra la entrada de voz
+     */
+    private void EscucharVoz() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What do u wanna do?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "no rula",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Recibe la entrada de voz
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    // Mostramos el mensaje con un toast
+                    Toast.makeText(getApplicationContext(),
+                            result.get(0),
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+
+        }
+    }
+}
+
+
